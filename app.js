4aa82adca65593e7efd3f5dd89ffd6ebad91365e
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-03-22 20:21:38 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB。mongoose.connect其實直接用連線字串就好，但因為連線字串裡有密碼，所以改用環境中載入；process.env 是 Node.js 提供的一個介面，可以調用宣告在系統層的環境變數。
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
---
+++ model/todo.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose')
+const Schema =mongoose.Schema({
+  name: {
+    type: String, // 資料型別是字串
+    required: true // 這是個必填欄位
+  },
+ done: {
+  type: Boolean
+  }
+})
+module.exports = mongoose.model('Todo', todoSchema)//mongoose.model 會複製我們定義的 Schema 並編譯成一個可供操作的 model 物件，匯出的時候我們把這份 model 命名為 Todo，以後在其他的檔案直接使用 Todo 就可以操作和「待辦事項」有關的資料了！
