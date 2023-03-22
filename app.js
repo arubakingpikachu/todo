761da9dff3a695012bfd2a72d628294b45c33842
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-03-22 22:08:43 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars');
+
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -9,6 +11,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB。mongoose.connect其實直接用連線字串就好，但因為連線字串裡有密碼，所以改用環境中載入；process.env 是 Node.js 提供的一個介面，可以調用宣告在系統層的環境變數。
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -24,6 +29,10 @@ db.once('open', () => {
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 // 設定 port 3000
