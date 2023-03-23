0f4715d1359c79f93a26fbf334b319bb09ed9a1a
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-03-23 10:18:31 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+const Todo=require('./models/todo')
 
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -26,13 +27,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()// 用find 取出 Todo model 裡的所有資料
+  .lean()//把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+  .then(todos=>res.render('index', { todos:todos }))// 將資料傳給 index 樣板
+  .catch(error => console.error(error))//錯誤處理
 })
 
 // 設定 port 3000
