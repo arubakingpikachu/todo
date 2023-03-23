967b3a0637ca502b96122bec78f5eb26610899a0
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-03-23 18:19:46 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const Todo=require('./models/todo')
+const bodyParser=require('body-parser')
 
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -27,11 +28,23 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))//body-parser，用來解析解析 request body的中介軟體
+
 app.get('/', (req, res) => {
   Todo.find()// 用find 取出 Todo model 裡的所有資料
   .lean()//把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
   .then(todos=>res.render('index', { todos:todos }))// 將資料傳給 index 樣板
   .catch(error => console.error(error))//錯誤處理
+})
+
+app.get('/todos/new',(req,res)=>{
+  return res.render('new')
+})
+app.post('/todos',(req,res)=>{
+  const name = req.body.name
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
